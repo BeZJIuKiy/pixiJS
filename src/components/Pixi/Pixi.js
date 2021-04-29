9515dead4c71020f5ba89edd6245fc4d1bfe7f0b
BeZJIuKiy <matveenkov.sasha@gmail.com>
2021-04-29 14:14:32 +0300

    new model STABLE

--- src/components/Pixi/Pixi.js
+++ src/components/Pixi/Pixi.js
@@ -8,35 +8,57 @@ import PixiState from '../Store/PixiState';
 
 // Тестовый проект
 
-export const Pixi = observer (() => {
+export const Pixi = observer(() => {
     const canvasRef = useRef();
-    
+    let _w = window.innerWidth;
+    let _h = window.innerHeight;
+
     useEffect(() => {
         PixiState.setCanvas(canvasRef.current);
-        
+
         main();
+
     }, []);
 
     const main = () => {
-        const app = new PIXI.Application({
-            view: canvasRef.current,
-            width: window.innerWidth,
-            height: window.innerHeight
+        const renderer = new PIXI.Renderer({
+            view: canvasRef.current,            // Ссылка на объект
+            width: _w,                          // Ширина
+            height: _h,                         // Высота
+            resolution: window.devicePixelRatio,
+            autoDensity: true,
         });
-    
+
+        window.addEventListener('resize', () => resize());
+
+        const resize = () => {
+            _w = window.innerWidth;
+            _h = window.innerHeight;
+
+            renderer.resize(_w, _h);
+        }
+
+        const stage = new PIXI.Container();     // Сюда все передается
+
         const texture = PIXI.Texture.from(ava);
         const img = new PIXI.Sprite(texture);
-    
-        img.x = app.renderer.width / 2;
-        img.y = app.renderer.height / 2;
-    
+
         img.anchor.x = 0.5;
         img.anchor.y = 0.5;
-    
-        app.stage.addChild(img);
-        app.ticker.add(() => animate(img));
 
-        const animate = (img => img.rotation += 0.01);
+        stage.addChild(img);
+
+        const ticker = new PIXI.Ticker();
+        ticker.add(() => animate());
+        ticker.start();
+
+        const animate = (() => {
+            img.x = renderer.screen.width / 2;
+            img.y = renderer.screen.height / 2;
+
+            img.rotation += 0.01;
+            renderer.render(stage);
+        });
     }
 
     return (
@@ -86,95 +108,4 @@ export const Pixi = () => {
     />
   );
 };
-*/
-
-
-// Падение кроликов :)
-/*
-const img = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png";
-
-const emitterConfig = {
-    alpha: {
-        start: 0.5,
-        end: 0.5
-    },
-    scale: {
-        start: 1,
-        end: 1
-    },
-    color: {
-        start: "ffffff",
-        end: "ffffff"
-    },
-    speed: {
-        start: 3000,
-        end: 3000
-    },
-    startRotation: {
-        min: 65,
-        max: 65
-    },
-    rotationSpeed: {
-        min: 0,
-        max: 0
-    },
-    lifetime: {
-        min: 0.81,
-        max: 0.81
-    },
-    blendMode: "normal",
-    frequency: 0.004,
-    emitterLifetime: 0,
-    maxParticles: 1000,
-    pos: {
-        x: 0,
-        y: 0
-    },
-    addAtBack: false,
-    spawnType: "rect",
-    spawnRect: {
-        x: -600,
-        y: -460,
-        w: 900,
-        h: 20
-    }
-};
-
-export const Pixi = PixiComponent("Emitter", {
-    create() {
-        return new PIXI.Container();
-    },
-    applyProps(instance, oldProps, newProps) {
-        const { image, config } = newProps;
-
-        if (!this._emitter) {
-            this._emitter = new PIXI.particles.Emitter(
-                instance,
-                [PIXI.Texture.from(image)],
-                config
-            );
-
-            let elapsed = Date.now();
-
-            const t = () => {
-                this._emitter.raf = requestAnimationFrame(t);
-                const now = Date.now();
-
-                this._emitter.update((now - elapsed) * 0.001);
-
-                elapsed = now;
-            };
-
-            this._emitter.emit = true;
-            t();
-        }
-    },
-
-    willUnmount() {
-        if (this._emitter) {
-            this._emitter.emit = false;
-            cancelAnimationFrame(this._emitter.raf);
-        }
-    }
-});
 */
