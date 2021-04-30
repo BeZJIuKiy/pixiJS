d436542370e6941c61e57d5755a3fa219561c93d
BeZJIuKiy <matveenkov.sasha@gmail.com>
2021-04-30 10:35:18 +0300

    added second img

--- src/components/Pixi/Pixi.js
+++ src/components/Pixi/Pixi.js
@@ -7,88 +7,89 @@ import ava2 from './images/ava2.jpg';
 import { observer } from 'mobx-react-lite';
 import PixiState from '../Store/PixiState';
 
+// const userAvatar = 'https://www.pngkey.com/png/full/572-5723307_kaneki-ken-kanekiken-kaneki-anime.png';
+
 // Тестовый проект
 export const Pixi = observer(() => {
-    const canvasRef = useRef();
-    const [curSize, setCurSize] = useState({
+  const canvasRef = useRef();
+  const [curSize, setCurSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  });
+
+  useEffect(() => {
+    PixiState.setCanvas(canvasRef.current);
+
+    main();
+  }, []);
+
+  const main = () => {
+    window.addEventListener('resize', () => resize());
+    const resize = () => {
+      setCurSize({
         width: window.innerWidth,
         height: window.innerHeight
-    });
-
-    useEffect(() => {
-        PixiState.setCanvas(canvasRef.current);
-
-        main();
-    }, []);
-
-    const main = () => {
-        window.addEventListener('resize', () => resize());
-        const resize = () => {
-            setCurSize({
-                width: window.innerWidth,
-                height: window.innerHeight
-            })
-            renderer.resize(window.innerWidth, window.innerHeight);
-        }
-
-        const renderer = new PIXI.Renderer({
-            view: canvasRef.current,                // Ссылка на объект
-            width: curSize.width,                   // Ширина
-            height: curSize.height,                 // Высота
-            resolution: window.devicePixelRatio,    // Разрешение
-            autoDensity: true,                      // Плотность
-        });
-
-        const loader = PIXI.Loader.shared;
-        // loader.onComplete.add(() => handleLoadComplete());
-        // loader.onLoad.add(() => handleLoadAsset());
-        // loader.onError.add(() => handleLoadError());
-        // loader.onProgress.add(() => handleLoadProgress(loader));
-        
-        loader.add(ava1)
-            .add(ava2)
-            .on("asset", (() => handleLoadAsset()))
-            .on("error", (() => handleLoadError()))
-            .on("progress", (() => handleLoadProgress(loader)))
-            .load(() => handleLoadComplete());
-
-        const handleLoadComplete = () => {
-            const texture = loader.resources[ava1].texture;
-            const image = new PIXI.Sprite(texture);
-            image.anchor.x = 0.5;
-            image.anchor.y = 0.5;
-
-            const stage = new PIXI.Container();
-            stage.addChild(image);
-
-            const ticker = new PIXI.Ticker();
-            ticker.add(() => animate(image, stage));
-            ticker.start();
-        }
-        const handleLoadAsset = () => {
-            console.log("asset loaded");
-        }
-        const handleLoadError = () => {
-            console.error("load error");
-        }
-        const handleLoadProgress = (loader, resource) => {
-            console.log(loader.progress + "% loaded");
-        }
-
-        const animate = ((img, stage) => {
-            img.x = renderer.screen.width / 2;
-            img.y = renderer.screen.height / 2;
-
-            img.rotation += 0.01;
-            renderer.render(stage);
-        });
+      })
+      renderer.resize(window.innerWidth, window.innerHeight);
     }
 
-    return (
-        <div>
-            <canvas ref={canvasRef} />
-        </div>
-    )
+    const renderer = new PIXI.Renderer({
+      view: canvasRef.current,                // Ссылка на объект
+      width: curSize.width,                   // Ширина
+      height: curSize.height,                 // Высота
+      resolution: window.devicePixelRatio,    // Разрешение
+      autoDensity: true,                      // Плотность
+    });
+
+    const loader = PIXI.Loader.shared;
+
+    loader
+      .add('mask', ava1)
+      .add('jonny', ava2)
+      .load(() => handleLoadComplete(loader, loader.resources));
+
+    loader.onLoad.add(() => handleLoadAsset(loader.resources));
+    loader.onError.add(() => handleLoadError());
+    loader.onProgress.add(() => handleLoadProgress(loader));
+
+    const handleLoadComplete = (loader, resources) => {
+      let image = new PIXI.Sprite(resources.mask.texture);
+      image.anchor.x = 0.5;
+      image.anchor.y = 0.5;
+
+      const stage = new PIXI.Container();
+      stage.addChild(image);
+
+      const ticker = new PIXI.Ticker();
+      ticker.add(() => animate(image, stage));
+      ticker.start();
+
+      setTimeout(() => image.texture = resources.jonny.texture, 2000)
+    }
+    const handleLoadAsset = (resources) => {
+      console.log('asset loaded');
+    }
+    const handleLoadError = () => {
+      console.error("load error");
+    }
+    const handleLoadProgress = (loader, resource) => {
+      console.log(loader.progress + "% loaded");
+    }
+
+    const animate = ((img, stage) => {
+      img.x = renderer.screen.width / 2;
+      img.y = renderer.screen.height / 2;
+
+      img.rotation += 0.01;
+      renderer.render(stage);
+    });
+  }
+
+  return (
+    <div>
+      <canvas ref={canvasRef} />
+    </div>
+  )
 })
 
 
